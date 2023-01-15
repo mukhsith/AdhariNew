c590ff119b0db48d6fe2201d5508ef8fb7857e8e
Mukhsith T <119151615+mukhsith@users.noreply.github.com>
2023-01-15 15:20:44 +0300

    Delete Data directory

--- Data/Common/BaseEntityCommon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Data.Common
-{
-    public partial class BaseEntityCommon : BaseEntityDate
-    {
-        public int DisplayOrder { get; set; } = 0;
-        public bool Active { get; set; } = false;
-    }
-}
